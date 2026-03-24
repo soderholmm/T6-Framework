6ca76931f5416738548605c399d43f3e6b3ac9f0
soderholmm <soderholm83@gmail.com>
2026-03-24 18:41:21 +0200

    fix: Some basic Copyright and text changes

--- tpl_t6_bs5_blank/templateInfo.php
+++ tpl_t6_bs5_blank/templateInfo.php
@@ -4,9 +4,10 @@
  * T6 Blank Template Bs5
  * ------------------------------------------------------------------------
  * Copyright (C) 2004-2018 J.O.O.M Solutions Co., Ltd. All Rights Reserved.
+ * @copyright (C) 2026 Ab Söderholms IT-tjänster Oy. All Rights Reserved.
  * @license - GNU/GPL, http://www.gnu.org/licenses/gpl.html
- * Author: J.O.O.M Solutions Co., Ltd
- * Websites: http://www.joomlart.com - http://www.joomlancers.com
+ * @author: SIT (Ab Söderholms IT-tjänster Oy)
+ * Websites: https://www.ittjanster.fi
  * ------------------------------------------------------------------------
 */
 
